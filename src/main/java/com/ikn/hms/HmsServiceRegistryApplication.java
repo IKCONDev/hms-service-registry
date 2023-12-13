0246aeb68b6b8c4bbfd4ee4c0a4dfdbeb784cff1
Bharat <Bharat@ikcontech.com>
2023-12-13 17:38:31 +0530

    first-commit

---
+++ src/main/java/com/ikn/hms/HmsServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.ikn.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsServiceRegistryApplication.class, args);
+	}
+
+}
